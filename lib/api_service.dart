62168b71405266c09f9f07e1c70add10ff50d89a
Cheba <arina.rybakovaa@gmail.com>
2024-04-22 15:13:42 +0300

    updating api

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -8,15 +8,17 @@ class ApiService {
   final String _baseUrl = 'http://localhost:8080';
   String responseAmount = '';
   String responsePeriod = '';
+  String responseCountryCode = '';
+  String responseDateOfBirth = '';
   String responseError = '';
   http.Client httpClient;
 
   ApiService({http.Client? client}) : httpClient = client ?? http.Client();
 
   // requestLoanDecision sends a request to the API to get a loan decision
-  // based on the provided personalCode, loanAmount, and loanPeriod.
+  // based on the provided personalCode, loanAmount, loanPeriod, countryCode, and dateOfBirth.
   Future<Map<String, String>> requestLoanDecision(
-      String personalCode, int loanAmount, int loanPeriod) async {
+      String personalCode, int loanAmount, int loanPeriod, String countryCode, String dateOfBirth) async {
     final response = await httpClient.post(
       Uri.parse('$_baseUrl/loan/decision'),
       headers: {'Content-Type': 'application/json'},
@@ -24,29 +26,37 @@ class ApiService {
         'personalCode': personalCode,
         'loanAmount': loanAmount,
         'loanPeriod': loanPeriod,
+        'countryCode': countryCode,
+        'dateOfBirth': dateOfBirth,
       }),
     );
 
-    try {
+    if (response.statusCode == 200) {
       // Decode the API response and update response data variables
       final responseData = jsonDecode(response.body) as Map<String, dynamic>;
-      responseAmount = responseData['loanAmount'].toString();
-      responsePeriod = responseData['loanPeriod'].toString();
-      responseError = responseData['errorMessage'].toString();
-
-      // Return the response data as a map, handling null values if necessary
       return {
-        'loanAmount': responseAmount != 'null' ? responseAmount : '0',
-        'loanPeriod': responsePeriod != 'null' ? responsePeriod : '0',
-        'errorMessage': responseError != 'null' ? responseError : '',
+        'loanAmount': responseData['loanAmount'].toString(),
+        'loanPeriod': responseData['loanPeriod'].toString(),
+        'countryCode': responseData['countryCode'].toString(),
+        'dateOfBirth': responseData['dateOfBirth'].toString(),
+        'errorMessage': responseData['errorMessage'] ?? '',
       };
-    } catch (e) {
-      // An unexpected error occurred when querying the server,
-      // so an error is displayed.
+    } else {
+      // Parse the response to get the error message from the server
+      Map<String, dynamic> responseData = jsonDecode(response.body);
+      String errorMessage = responseData['errorMessage'] ?? 'An unexpected error occurred.';
+
+      // Provide more context based on the status code
+      if (response.statusCode == 400) {
+        errorMessage = errorMessage;
+      } else if (response.statusCode == 404) {
+        errorMessage = 'Unfortunately, we could not find an available loan for you.';
+      } else if (response.statusCode == 500) {
+        errorMessage = 'Server error: ' + errorMessage;
+      }
+
       return {
-        'loanAmount': '0',
-        'loanPeriod': '0',
-        'errorMessage': 'An unexpected error occurred.',
+        'errorMessage': errorMessage
       };
     }
   }
